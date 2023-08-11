8aa332921168cc804e773052d538ab063927152a
Olasande <Olasandeniola@gmail.com>
2023-08-10 18:18:27 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main-the program runs through
+ * return 0 (sucess)
+ * /
+ int main(void) {
+ puts("\nProgramming is like building a multilingual puzzle");
+ return (0);
+ }
